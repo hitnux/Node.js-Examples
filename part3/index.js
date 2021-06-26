58db05224ff0f423a37adad196a112d1493581f1
Halil Bilgin <hlilbilgin@gmail.com>
2021-06-26 14:28:22 +0300

    Create index.js

---
+++ part3/index.js
@@ -0,0 +1,6 @@
+const { circleArea, circleCircumference } = require('./circle.js');
+
+let r = 5;
+
+console.log('Dairenin alanı: '+circleArea(r));
+console.log('Dairenin çevresi: '+circleCircumference(r));
